bf3ee57cfeda367a1f17bba5c9bda5debcf97e0f
dpes44 <dpes.adikari44@gmail.com>
2025-08-12 13:38:37 +0545

    config file update

--- src/sdk/userConfig.js
+++ src/sdk/userConfig.js
@@ -1,54 +1,52 @@
-import {
-  debug_Notifications,
-  user_Preference_Turn_Off_All_Notifications,
-  Turn_Off_All_Notifications_Time_Unix,
-  user_Preference_Posts,
-  user_Preference_Post_Comments,
-  user_Preference_Comments_On_My_Posts,
-  user_Preference_Submissions,
-  user_Preference_Submission_Comments,
-  user_Preference_Comments_On_My_Submissions,
-  user_Preference_Announcements,
-  user_Preference_Announcement_Comments,
-  user_Preference_Comments_On_My_Announcements,
-  user_Preference_Announcement_Mentions,
-  user_Preference_Announcement_Comment_Mentions,
-  user_Preference_Post_Mentions,
-  user_Preference_Post_Comment_Mentions,
-  user_Preference_Submission_Mentions,
-  user_Preference_Submission_Comment_Mentions
-} from './config.js';
+const debug_Notifications = false;
+const user_Preference_Turn_Off_All_Notifications = 'No';
+const Turn_Off_All_Notifications_Time_Unix = '';
+const user_Preference_Posts = 'Yes';
+const user_Preference_Submissions = 'Yes';
+const user_Preference_Announcements = 'Yes';
+const user_Preference_Post_Comments = 'Yes';
+const user_Preference_Submission_Comments = 'Yes';
+const user_Preference_Announcement_Comments = 'Yes';
+const user_Preference_Comments_On_My_Posts = 'Yes';
+const user_Preference_Comments_On_My_Submissions = 'Yes';
+const user_Preference_Comments_On_My_Announcements = 'Yes';
+const user_Preference_Post_Mentions = 'Yes';
+const user_Preference_Announcement_Mentions = 'Yes';
+const user_Preference_Submission_Mentions = 'Yes';
+const user_Preference_Announcement_Comment_Mentions = 'Yes';
+const user_Preference_Post_Comment_Mentions = 'Yes';
+const user_Preference_Submission_Comment_Mentions = 'Yes';
 
 export class UserConfig {
-    constructor() {
-        this.userId = window.loggedinuserid ??= 62;
-        this.userType = window.loggedinuserType ??= "Student";
-        this.debug = {
-          notifications: debug_Notifications,
-        };
+  constructor() {
+    this.userId = window.loggedinuserid ??= 62;
+    this.userType = window.loggedinuserType ??= "Student";
+    this.debug = {
+      notifications: debug_Notifications,
+    };
 
-        // Preferences (merge field values resolved already into constants)
-        this.preferences = {
-            turnOffAllNotifications: user_Preference_Turn_Off_All_Notifications, //Stops fetching notifications completely
-            turnOffAllNotificationsTimeUnix: Turn_Off_All_Notifications_Time_Unix, //We will not use it now
+    // Preferences (merge field values resolved already into constants)
+    this.preferences = {
+      turnOffAllNotifications: user_Preference_Turn_Off_All_Notifications, //Stops fetching notifications completely
+      turnOffAllNotificationsTimeUnix: Turn_Off_All_Notifications_Time_Unix, //We will not use it now
 
-            posts: user_Preference_Posts, //Fetch only posts type        
-            submissions: user_Preference_Submissions, //Fetch only submissions type
-            announcements: user_Preference_Announcements, //Fetch only announcements type
+      posts: user_Preference_Posts, //Fetch only posts type        
+      submissions: user_Preference_Submissions, //Fetch only submissions type
+      announcements: user_Preference_Announcements, //Fetch only announcements type
 
-            postComments: user_Preference_Post_Comments, //Fetch only post comments type
-            commentsOnMyPosts: user_Preference_Comments_On_My_Posts, //Fetch only comments on my posts type
-            submissionComments: user_Preference_Submission_Comments, //Fetch only submission comments type
-            commentsOnMySubmissions: user_Preference_Comments_On_My_Submissions, //Fetch only comments on my submissions type
-            announcementComments: user_Preference_Announcement_Comments, //Fetch only announcement comments type
-            commentsOnMyAnnouncements: user_Preference_Comments_On_My_Announcements, //Fetch only comments on my announcements type
+      postComments: user_Preference_Post_Comments, //Fetch only post comments type
+      commentsOnMyPosts: user_Preference_Comments_On_My_Posts, //Fetch only comments on my posts type
+      submissionComments: user_Preference_Submission_Comments, //Fetch only submission comments type
+      commentsOnMySubmissions: user_Preference_Comments_On_My_Submissions, //Fetch only comments on my submissions type
+      announcementComments: user_Preference_Announcement_Comments, //Fetch only announcement comments type
+      commentsOnMyAnnouncements: user_Preference_Comments_On_My_Announcements, //Fetch only comments on my announcements type
 
-            postMentions: user_Preference_Post_Mentions, //Fetch only post mentions type
-            postCommentMentions: user_Preference_Post_Comment_Mentions, //Fetch only post comment mentions type
-            submissionMentions: user_Preference_Submission_Mentions, //Fetch only submission mentions type
-            submissionCommentMentions: user_Preference_Submission_Comment_Mentions, //Fetch only submission comment mentions type
-            announcementMentions: user_Preference_Announcement_Mentions, //Fetch only announcement mentions type
-            announcementCommentMentions: user_Preference_Announcement_Comment_Mentions, //Fetch only announcement comment mentions type
-        };
-    }
+      postMentions: user_Preference_Post_Mentions, //Fetch only post mentions type
+      postCommentMentions: user_Preference_Post_Comment_Mentions, //Fetch only post comment mentions type
+      submissionMentions: user_Preference_Submission_Mentions, //Fetch only submission mentions type
+      submissionCommentMentions: user_Preference_Submission_Comment_Mentions, //Fetch only submission comment mentions type
+      announcementMentions: user_Preference_Announcement_Mentions, //Fetch only announcement mentions type
+      announcementCommentMentions: user_Preference_Announcement_Comment_Mentions, //Fetch only announcement comment mentions type
+    };
+  }
 }
